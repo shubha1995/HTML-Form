62a396e29d1f5ff8f5839c02598dff639d8c0cad
shubhasankar <ssbhaumikdmr@gmail.com>
2021-08-11 07:35:50 +0530

    Added: Pnone Number Validation

--- App.js
+++ App.js
@@ -23,11 +23,26 @@ const email = document.querySelector('#email');
 const emailError = document.querySelector('.email-error');
 email.addEventListener('input', function () {
     let emailRegex = RegExp(
-        "^[a-zA-Z0-9_+&*#$^!-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$"
+        "^[a-zA-Z0-9_+&*#$^!-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{" +
+        "2,7}$"
     );
     if (emailRegex.test(email.value)) 
         emailError.emailContent = ' ';
     else 
         emailError.emailContent = "Email is InValid";
+    }
+);
+
+// Validate  phone number using the REGEX expression
+
+const number = document.querySelector('#tel');
+const numberError = document.querySelector('.tel-error');
+number.addEventListener('input', function () {
+    let numberRegex = RegExp('^[1-9]{2}\\s{0,1}[1-9]{1}[0-9]{9}$');
+    if (numberRegex.test(number.value)) {
+        console.log("correct");
+        numberError.textContent = ' ';
+    } else 
+        numberError.textContent = 'Invalid Number!';
     }
 );
